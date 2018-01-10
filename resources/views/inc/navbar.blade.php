571a0ab8de113ba3b3e03e78983eea41de46e739
Webster Genise <kaykaygenise@gmail.com>
2018-01-10 14:56:04 +0800

    fixed navbar final

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -11,7 +11,6 @@
     <ul class="collapsible collapsible-accordion">
       <li>
         <a class="collapsible-header " onclick="Materialize.showStaggeredList('#trans')"><h5>Projects</h5></a>
-        <a class="collapsible-header" onclick="Materialize.showStaggeredList('#trans')"><h5>Projects</h5></a>
         <div class="collapsible-body grey lighten-2">
           <ul id="trans">
             <li><a href="{{url('addprojects')}}">Add Project</a></li>
