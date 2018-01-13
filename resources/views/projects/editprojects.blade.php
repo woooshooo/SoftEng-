b4caed6f39136da5eb603c29c8884ddeb8757028
fonzobalao <34019905+fonzobalao@users.noreply.github.com>
2018-01-13 22:35:28 +0800

    with ui for project
    With UI for view projects

---
+++ resources/views/projects/editprojects.blade.php
@@ -0,0 +1,9 @@
+@extends('layout.app')
+@section('content')
+    <div class="container">
+        <div class="row">
+            <h1 class="page-header">Edit Project</h1>
+        </div>
+    </div>
+        {!! Form::open(['action' => ])}
+@endsection
--- resources/views/projects/projects.blade.php
+++ resources/views/projects/projects.blade.php
@@ -1,6 +1,17 @@
 @extends('layout.app')
 @section('content')
 		<div class="container">
-      <h1>Viewing a Projects</h1>
-    </div>
+            <h1 class="page-header">Projects</h1>
+            <table width="100%" class="table table-bordered table-hover table-responsive" id="dataTables-example">
+                <thead>
+                    <tr>
+                    <th>Date</th>
+                    <th>Project Name</th>
+                    <th>Client</th>
+                    <th>Status</th>
+                    <th></th>
+                    </tr>
+                </thead>
+            </table>
+        </div>
 @endsection
