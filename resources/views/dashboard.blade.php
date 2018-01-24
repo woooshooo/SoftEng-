3d5928b85ac1fb11dc959e4d34e64ee1049cd85a
fonzobalao <34019905+fonzobalao@users.noreply.github.com>
2018-01-25 00:21:02 +0800

    subtle changes in dashboard
    changes in icons in dashboard nav cards

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -37,7 +37,7 @@
                         <div class="panel-heading">
                             <div class="row">
                                 <div class="col-xs-3">
-                                    <i class="fa fa-tasks fa-5x"></i>
+                                    <i class="fa fa-book     fa-5x"></i>
                                 </div>
                                 <div class="col-xs-9 text-right">
                                     <div class="huge">{{$events}}</div>
@@ -59,7 +59,7 @@
                         <div class="panel-heading">
                             <div class="row">
                                 <div class="col-xs-3">
-                                    <i class="fa fa-shopping-cart fa-5x"></i>
+                                    <i class="fa fa-user fa-5x"></i>
                                 </div>
                                 <div class="col-xs-9 text-right">
                                     <div class="huge">{{$staffs}}</div>
@@ -81,7 +81,7 @@
                         <div class="panel-heading">
                             <div class="row">
                                 <div class="col-xs-3">
-                                    <i class="fa fa-support fa-5x"></i>
+                                    <i class="fa fa-male fa-5x"></i>
                                 </div>
                                 <div class="col-xs-9 text-right">
                                     <div class="huge">{{$vols}}</div>
